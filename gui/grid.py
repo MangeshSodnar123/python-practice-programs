827053f1c59350ce6ebfd6134c42a9afad3f3007
Mangesh <mangeshsodnar123@gmail.com>
2021-09-04 19:22:06 +0530

    tkinter codes

---
+++ gui/grid.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+myLabel1 = Label(root, text="Hello World").grid(row=0, column=0)
+myLabel2 = Label(root, text="Hi this is Mangesh").grid(row=1, column=1)
+
+
+root.mainloop()
---
+++ gui/lesson1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+# creating a label widget 
+myLabel = Label(root, text="Hello World")
+# showing it onto a screen
+myLabel.pack()
+
+root.mainloop()
